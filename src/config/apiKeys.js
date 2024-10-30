12ee940f6972643014e43041f0cfb47b083298c6
jonathanphill13 <jonathanphill@gmail.com>
2024-10-29 22:21:11 -0500

    updated gitIgnore in main

--- src/config/apiKeys.js
+++
@@ -1,6 +0,0 @@
-export const apiKey = "AIzaSyBOjE_rbvxW4NTiCj4Qc3etwrCEfusOHBM";
-export const authDomain = "fryer-counter.firebaseapp.com";
-
-export const messagingSenderId = "867746060552";
-
-export const appId = "1:867746060552:web:c262504a9d945b932034bd";
