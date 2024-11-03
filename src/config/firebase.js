cead499408096c625bcf08fbfcd7c3d1eeb02e90
jonathanphill13 <jonathanphill@gmail.com>
2024-11-02 19:15:05 -0500

    add an .env file and used the import for the same for the firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { apiKey, authDomain, messagingSenderId, appId, projectId, storageBucket } from "./apiKeys";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket:storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+export default firebaseConfig;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
